3c2d542383e85cfc1ceda43b81bb7b20fb7b93d5
Jenskiii <124568361+Jenskiii@users.noreply.github.com>
2024-02-28 15:20:30 +0100

    added border on active site

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 // selectors
-const hamburger = document.querySelector(".nav-hamburger")
-const navigation = document.getElementById("header-navigation")
-const navigationItems = document.querySelectorAll("#header-navigation li")
+const hamburger = document.querySelector(".nav-hamburger");
+const navigation = document.getElementById("header-navigation");
+const navigationItems = document.querySelectorAll("#header-navigation li");
 
 //  opens / closes mobile menu on click
-function handleClick(){
-    hamburger.classList.toggle("active")
-    navigation.classList.toggle("active") 
+function handleClick() {
+  hamburger.classList.toggle("active");
+  navigation.classList.toggle("active");
 }
 
 // active function on click
-hamburger.addEventListener("click", handleClick)
-
+hamburger.addEventListener("click", handleClick);
 
 // closes menu on menu item click
-navigationItems.forEach(e => {
-    e.addEventListener("click" , () =>{
-        navigation.classList.remove("active")
-    })
+navigationItems.forEach((e) => {
+  e.addEventListener("click", () => {
+    navigation.classList.remove("active");
+    hamburger.classList.remove("active");
+  });
 });
